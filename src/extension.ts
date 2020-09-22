d5cf3e81719c63dc7791a4c32a1f132a6d78bcd8
pengjiasheng <pengjiasheng@cvte.com>
2020-09-22 11:50:07 +0800

    refactor: debug

--- src/extension.ts
+++ src/extension.ts
@@ -1,77 +1,141 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-interface IConfig {
-  moduleName: string,
-  dependency: string,
-  allowSyntheticDefaultImports?: boolean,
-  entireModuleContents?: boolean
+const react = require('../snippets/react.json');
+const imports = require('../snippets/imports.json');
+
+const getEmptyLineNumber = () => {
+  const document = vscode.window.activeTextEditor?.document as vscode.TextDocument;
+  
+  const lineCount = document.lineCount;
+
+  for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+    let lineText = document.lineAt(lineNumber);
+    if (lineText.text.match(/\S|/g)) return lineNumber;
+  }
+
+  return 0;
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ragnarok-snippets-plugin" is now active!');
+  console.log('Congratulations, your extension "vscode-ragnarok-snippets-plugin" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ragnarok-snippets-plugin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  const snippetsCommand = async (tpl: any) => {
+    const options = {
+      matchOnDescription: true,
+      matchOnDetail: true,
+      placeHolder: 'Loading Snippets (pick one...)',
+    }
 
-		// Display a message box to the user
-    // vscode.window.showInformationMessage('Hello World from vscode-ragnarok-snippets-plugin!');
+    const snippetsArray = Object.entries(tpl);
+    const items = snippetsArray.map(
+      ([shortDescription, content], index) => {
+        const { title, prefix, body, description, dependencies } = content as any;
+        const value = typeof prefix === 'string' ? prefix : prefix[0]
+
+        return {
+          id: index,
+          description: description || shortDescription,
+          label: title || value,
+          value,
+          body,
+          dependencies
+        }
+      }
+    )
+    const snippet: any = await vscode.window.showQuickPick(items, options);
+    const activeTextEditor = vscode.window.activeTextEditor;
+    const body = typeof snippet.body === 'string' ? snippet.body : snippet.body.join('\n');
+
+    await activeTextEditor?.insertSnippet(new vscode.SnippetString(body));
+  }
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-ragnarok-snippets-plugin.snippetsReact', () => {
+    snippetsCommand(react);
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('vscode-ragnarok-snippets-plugin.snippetsImport', async () => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+    const options = {
+      matchOnDescription: true,
+      matchOnDetail: true,
+      placeHolder: 'Import package',
+    }
+
+    const snippetsArray = Object.entries(imports);
+    const items = snippetsArray.map(
+      ([shortDescription, content], index) => {
+        const {
+          title,
+          moduleName,
+          dependency,
+          ImportExportsFromModule = false,
+          ImportAnEntireModulesContents = false,
+          ImportingDefaults = false
+        } = content as any;
+
+        return {
+          id: index,
+          description: shortDescription,
+          label: title || moduleName || shortDescription,
+          body: '',
+          moduleName,
+          dependency,
+          ImportExportsFromModule,
+          ImportAnEntireModulesContents,
+          ImportingDefaults
+        }
+      }
+    )
+    const snippet: any = await vscode.window.showQuickPick(items, options);
 
     const activeTextEditor = vscode.window.activeTextEditor;
-    const mergeImports = (config: IConfig) => {
-      const {moduleName, dependency, allowSyntheticDefaultImports, entireModuleContents} = config;
-      const edit: vscode.WorkspaceEdit = new vscode.WorkspaceEdit();
-      const DRegexp = new RegExp(`(?:import)(.|\\s)*(?:from)(.|\\s)*\\W(?:${dependency})(?:\'|\")`);
-      const MDRegexp = new RegExp(`(?:import)(.|\\s)*\\W(?:${moduleName})\\W(.|\\s)*(?:from)(.|\\s)*\\W(?:${dependency})(?:\'|\")`);
-      let currentDoc = vscode.window.activeTextEditor?.document.getText();
-      
-      const isExistImport = currentDoc?.match(MDRegexp);
-      // 导入模块已经存在
-      if (isExistImport && isExistImport.length > 0) return;
+    const {
+      moduleName,
+      dependency,
+      ImportAnEntireModulesContents,
+      ImportingDefaults
+    } = snippet;
+    const edit: vscode.WorkspaceEdit = new vscode.WorkspaceEdit();
+    const DRegexp = new RegExp(`(?:import)(.|\\s)*{(.|\\s)*}(.|\\s)*(?:from)(.|\\s)*\\W(?:${dependency})(?:\'|\")`);
+    const MDRegexp = new RegExp(`(?:import)(.|\\s)*\\W(?:${moduleName})\\W(.|\\s)*(?:from)(.|\\s)*\\W(?:${dependency})(?:\'|\")`);
+    let currentDoc = vscode.window.activeTextEditor?.document.getText();
+    const document = vscode.window.activeTextEditor?.document as vscode.TextDocument;
+    const isExistImport = currentDoc?.match(MDRegexp);
+    
+    // 导入模块已经存在
+    if (isExistImport && isExistImport.length > 0) return;
 
-      // 依赖是否存在
-      const foundImport = currentDoc?.match(DRegexp);
+    // 依赖是否存在
+    const foundImport = currentDoc?.match(DRegexp);
+    
+    // 依赖不存在，则创建导入语句
+    if (!foundImport) {
+      const emptyLineNumber = getEmptyLineNumber();
       
-      // 依赖不存在，则创建导入语句
-      if (!foundImport) {
-        if (allowSyntheticDefaultImports) {
-          return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import ${moduleName} from "${dependency}";`), new vscode.Position(0, 0));
-        }
-  
-        if (entireModuleContents) {
-          return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import * as ${moduleName} from "${dependency}";`), new vscode.Position(0, 0));
-        }
-
-        return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import { ${moduleName} } from "${dependency}";`), new vscode.Position(0, 0));
+      if (ImportingDefaults) {
+        return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import ${moduleName} from "${dependency}";\n`), new vscode.Position(emptyLineNumber, 0));
       }
 
-      // 依赖存在，则改写导入语句
-      let nextImport = foundImport[0]
-        .replace(/{|}|from|import|'|"|[^A-Za-z0-9_,]|;/gi, '')
-        .replace(dependency, '')
-        .split(',')
-        .concat(moduleName)
+      if (ImportAnEntireModulesContents) {
+        return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import * as ${moduleName} from "${dependency}";\n`), new vscode.Position(emptyLineNumber, 0));
+      }
 
-      currentDoc = currentDoc?.replace(DRegexp, `import { ${nextImport.join(', ')} } from "${dependency}"`);
+      return activeTextEditor?.insertSnippet(new vscode.SnippetString(`import { ${moduleName} } from "${dependency}";\n`), new vscode.Position(emptyLineNumber, 0));
+    }
 
-      edit.replace((vscode.window.activeTextEditor?.document as vscode.TextDocument).uri, new vscode.Range(new vscode.Position(0, 0), new vscode.Position(99999, 99999)), currentDoc as string);
-      vscode.workspace.applyEdit(edit);
-    };
+    // 依赖存在，则改写导入语句
+    let nextImport = foundImport[0]
+      .replace(/{|}|from|import|'|"|[^A-Za-z0-9_,]|;/gi, '')
+      .replace(dependency, '')
+      .split(',')
+      .concat(moduleName)
 
-    mergeImports({moduleName: 'R', dependency: 'ramda'});
-	});
+    currentDoc = currentDoc?.replace(DRegexp, `import { ${nextImport.join(', ')} } from "${dependency}"`);
 
-	context.subscriptions.push(disposable);
+    edit.replace(document.uri, new vscode.Range(new vscode.Position(0, 0), new vscode.Position(99999, 99999)), currentDoc as string);
+    return vscode.workspace.applyEdit(edit);
+  }));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
